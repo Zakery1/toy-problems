c2d2798ca1f583529fe564cdc4291813982fc0be
Zachary Graham <zakgraham@gmail.com>
2020-10-08 23:32:45 -0700

    add scrabble hand

---
+++ scrabbleHand/scrabbleHand.js
@@ -0,0 +1,27 @@
+// Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
+
+// Here's an example hand:
+
+// [
+//   { tile: "N", score: 1 },
+//   { tile: "K", score: 5 },
+//   { tile: "Z", score: 10 },
+//   { tile: "X", score: 8 },
+//   { tile: "D", score: 2 },
+//   { tile: "A", score: 1 },
+//   { tile: "E", score: 1 }
+// ]
+// The players maximumScore from playing all these tiles would be 1 + 5 + 10 + 8 + 2 + 1 + 1, or 28.
+
+
+function maximumScore(tileHand) {
+	let total = 0;
+	for(let i = 0; i < tileHand.length ; i++) {
+		total+= +tileHand[i].score;
+	}
+	return total;
+}
+
+function maximumScore(tileHand) {
+	return tileHand.reduce((p,c) => p + c.score, 0);
+}
